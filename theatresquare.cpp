afb0ffdaddb9f2fae099eaa1614d74fb80a4bb25
Morgan Sun <64807003+sunmorgan@users.noreply.github.com>
2021-06-22 20:44:43 -0400

    Create theatresquare.cpp

---
+++ theatresquare.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+ 
+ 
+using namespace std;
+ 
+int main() {
+    ll n, m, a, t;
+    cin>>n>>m>>a;
+ 
+     cout<<(((n+a-1)/a)*((m+a-1)/a));
+ 
+    return 0;
+}
