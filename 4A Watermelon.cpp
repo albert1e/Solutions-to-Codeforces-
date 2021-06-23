6404701a09c087c31f9357df5f332505938c8136
albertsun <64807003+albert1e@users.noreply.github.com>
2021-06-22 20:41:10 -0400

    Create 4A Watermelon.cpp

---
+++ 4A Watermelon.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+ 
+ 
+using namespace std;
+ 
+int main() {
+    ll n{};
+    cin>>n;
+    
+    if(n%2==0&&n>2) {
+        cout<<"YES";
+    } else {
+        cout<<"NO";
+    }
+ 
+    return 0;
+}
