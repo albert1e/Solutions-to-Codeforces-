20f8790402fed1c25da09f5453bd6b1410019a65
Morgan Sun <64807003+sunmorgan@users.noreply.github.com>
2021-06-23 12:41:56 -0400

    Create (A) Domino piling.cpp

---
+++ (A) Domino piling.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+ 
+ 
+using namespace std;
+ 
+int main() {
+    int m, n, d(2);
+    
+    cin>>m>>n;
+ 
+    cout<<floor(m*n/d);
+ 
+    return 0;
+}
